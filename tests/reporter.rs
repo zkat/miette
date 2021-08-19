f49b7092101f5ef6a23bb3469d618cae65ff6049
Kat Marchán <kzm@zkat.tech>
2021-08-18 23:28:18 -0700

    rewrite test

--- tests/reporter.rs
+++ tests/reporter.rs
@@ -1,39 +1,20 @@
-use miette::{Diagnostic, DiagnosticReport, DiagnosticSnippet, MietteError, SourceSpan};
+use miette::{Diagnostic, DiagnosticReport, MietteError, SourceSpan};
 use thiserror::Error;
 
-#[derive(Debug, Error)]
-#[error("oops!")]
-struct MyBad {
-    message: String,
-    src: String,
-    ctx: SourceSpan,
-    highlight: SourceSpan,
-}
-
-impl Diagnostic for MyBad {
-    fn code(&self) -> Box<dyn std::fmt::Display> {
-        Box::new(&"oops::my::bad")
-    }
-
-    fn help(&self) -> Option<Box<dyn std::fmt::Display>> {
-        Some(Box::new(&"try doing it better next time?"))
-    }
-
-    fn snippets(&self) -> Option<Box<dyn Iterator<Item = DiagnosticSnippet> + '_>> {
-        Some(Box::new(
-            vec![DiagnosticSnippet {
-                message: Some(self.message.as_ref()),
-                source: &self.src,
-                context: self.ctx.clone(),
-                highlights: Some(vec![self.highlight.clone()]),
-            }]
-            .into_iter(),
-        ))
-    }
-}
-
 #[test]
-fn fancy() -> Result<(), MietteError> {
+fn single_line_snippet() -> Result<(), MietteError> {
+    #[derive(Debug, Diagnostic, Error)]
+    #[error("oops!")]
+    #[diagnostic(code(oops::my::bad), help("try doing it better next time?"))]
+    struct MyBad {
+        message: String,
+        src: String,
+        #[snippet(src, "This is the part that broke")]
+        ctx: SourceSpan,
+        #[highlight(ctx)]
+        highlight: SourceSpan,
+    }
+
     let src = "source\n  text\n    here".to_string();
     let len = src.len();
     let err = MyBad {
@@ -44,7 +25,7 @@ fn fancy() -> Result<(), MietteError> {
     };
     let rep: DiagnosticReport = err.into();
     let out = format!("{:?}", rep);
-    println!("{}", out);
-    assert_eq!("Error[oops::my::bad]: oops!\n\n[bad_file.rs] This is the part that broke:\n\n    1  | source\n    2  |   text\n    ⫶  |   ^^^^ this bit here\n    3  |     here\n\n﹦try doing it better next time?\n".to_string(), out);
+    // println!("{}", out);
+    assert_eq!("Error[oops::my::bad]: oops!\n\n[bad_file.rs] This is the part that broke:\n\n 1 │ source\n 2 │   text\n   ·   ──┬─\n   ·     ╰── this bit here\n 3 │     here\n\n﹦try doing it better next time?\n".to_string(), out);
     Ok(())
 }
