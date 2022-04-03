b4a9d4cd9bc43720613b7d2bb6b521d51922c6b8
Finomnis <Finomnis@users.noreply.github.com>
2022-04-03 19:41:54 +0200

    feat(report): Add conversion from Report to Box<dyn Error> (#149)

--- tests/test_convert.rs
+++ tests/test_convert.rs
@@ -21,3 +21,22 @@ fn test_question_mark() -> Result<(), Box<dyn Diagnostic>> {
     f()?;
     Ok(())
 }
+
+#[test]
+fn test_convert_stderr() {
+    let has_dropped = Flag::new();
+    let error: Report = Report::new(DetectDrop::new(&has_dropped));
+    let box_dyn = Box::<dyn std::error::Error + Send + Sync>::from(error);
+    assert_eq!("oh no!", box_dyn.to_string());
+    drop(box_dyn);
+    assert!(has_dropped.get());
+}
+
+#[test]
+fn test_question_mark_stderr() -> Result<(), Box<dyn std::error::Error>> {
+    fn f() -> Result<()> {
+        Ok(())
+    }
+    f()?;
+    Ok(())
+}
