9ed3ee3bec88073f52b79267abc6b859fef73875
Matthias Beyer <mail@beyermatthias.de>
2022-04-28 17:46:57 +0200

    Add derive macro implementation for diagnostic_source
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- miette-derive/src/lib.rs
+++ miette-derive/src/lib.rs
@@ -6,6 +6,7 @@ use diagnostic::Diagnostic;
 mod code;
 mod diagnostic;
 mod diagnostic_arg;
+mod diagnostic_source;
 mod fmt;
 mod forward;
 mod help;
@@ -16,7 +17,10 @@ mod source_code;
 mod url;
 mod utils;
 
-#[proc_macro_derive(Diagnostic, attributes(diagnostic, source_code, label, related, help))]
+#[proc_macro_derive(
+    Diagnostic,
+    attributes(diagnostic, source_code, label, related, help, diagnostic_source)
+)]
 pub fn derive_diagnostic(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let cmd = match Diagnostic::from_derive_input(input) {
