0b32574d5bbbc302ddfe2dc914d97874c0acd2f9
Matthias Beyer <mail@beyermatthias.de>
2022-05-13 08:06:00 +0200

    Add compiletest for enum error types
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- tests/test_diagnostic_source_macro.rs
+++ tests/test_diagnostic_source_macro.rs
@@ -18,3 +18,22 @@ fn test_diagnostic_source() {
     };
     assert!(error.diagnostic_source().is_some());
 }
+
+// Compiletest this:
+#[derive(thiserror::Error, miette::Diagnostic, Debug)]
+enum EnumError {
+    #[error("Some Error text")]
+    SyntaxErr(#[from] AnErr),
+}
+
+// Compiletest this:
+#[derive(thiserror::Error, miette::Diagnostic, Debug)]
+enum AnotherEnumError {
+    #[error("Some other Error Text")]
+    SyntaxErr {
+        #[from]
+        #[source]
+        #[diagnostic_source]
+        source: AnErr,
+    },
+}
