460de635e0904619e567c8c92287722aa56adc10
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-08-19 18:02:45 -0700

    Port the fix for miri failure from anyhow

--- src/eyreish/fmt.rs
+++ src/eyreish/fmt.rs
@@ -1,18 +1,20 @@
-use super::error::ErrorImpl;
+use super::{error::ErrorImpl, ptr::Ref};
 use core::fmt;
 
 impl ErrorImpl<()> {
-    pub(crate) fn display(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.handler
+    pub(crate) unsafe fn display(this: Ref<'_, Self>, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        this.deref()
+            .handler
             .as_ref()
-            .map(|handler| handler.display(self.error(), f))
-            .unwrap_or_else(|| core::fmt::Display::fmt(self.diagnostic(), f))
+            .map(|handler| handler.display(Self::error(this), f))
+            .unwrap_or_else(|| core::fmt::Display::fmt(Self::diagnostic(this), f))
     }
 
-    pub(crate) fn debug(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.handler
+    pub(crate) unsafe fn debug(this: Ref<'_, Self>, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        this.deref()
+            .handler
             .as_ref()
-            .map(|handler| handler.debug(self.diagnostic(), f))
-            .unwrap_or_else(|| core::fmt::Debug::fmt(self.diagnostic(), f))
+            .map(|handler| handler.debug(Self::diagnostic(this), f))
+            .unwrap_or_else(|| core::fmt::Debug::fmt(Self::diagnostic(this), f))
     }
 }
