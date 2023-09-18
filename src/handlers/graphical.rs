379514c77dbca1f027357b71b47d50107659f1fd
Bruce Mitchener <bruce.mitchener@gmail.com>
2023-09-18 21:17:00 +0700

    clippy: Remove explicit lifetime that can be elided.

--- src/handlers/graphical.rs
+++ src/handlers/graphical.rs
@@ -382,10 +382,10 @@ impl GraphicalReportHandler {
         Ok(())
     }
 
-    fn render_context<'a>(
+    fn render_context(
         &self,
         f: &mut impl fmt::Write,
-        source: &'a dyn SourceCode,
+        source: &dyn SourceCode,
         context: &LabeledSpan,
         labels: &[LabeledSpan],
     ) -> fmt::Result {
