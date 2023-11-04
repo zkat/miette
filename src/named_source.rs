2906e58b59b1b3563bb81f0452a9a14945fab4c9
Adam Curtis <kallisti.dev@gmail.com>
2023-11-04 13:28:35 -0400

    remove redundant NamedSource::name (shadows new SourceCode::name)

--- src/named_source.rs
+++ src/named_source.rs
@@ -27,11 +27,6 @@ impl NamedSource {
         }
     }
 
-    /// Gets the name of this `NamedSource`.
-    pub fn name(&self) -> &str {
-        &self.name
-    }
-
     /// Returns a reference the inner [`SourceCode`] type for this
     /// `NamedSource`.
     pub fn inner(&self) -> &(dyn SourceCode + 'static) {
